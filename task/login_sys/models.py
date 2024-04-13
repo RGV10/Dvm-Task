b7b00509fefb23b4f937b9f5c5009cd6f2cfba5e
RGV10 <68089180+RGV10@users.noreply.github.com>
2024-04-13 20:41:45 +0530

    Delete task directory

--- task/login_sys/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-# Create your models here.
-
-class CustomUser(AbstractUser):
-	check_prof = models.BooleanField(default=False)
-	
